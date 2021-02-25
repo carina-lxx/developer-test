6c8309b53a7f8a052b119b3100894612bb24c678
codingfox <carinalxx@gmail.com>
2021-02-24 19:16:13 -0800

    update redux reducer

--- src/redux/actions.jsx
+++ src/redux/actions.jsx
@@ -1,5 +1,6 @@
 export const ADD_ITEM = 'ADD_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
+export const RESET_LIST = 'RESET_LIST';
 
 export const addItem = (item) => {
   return {
@@ -14,3 +15,10 @@ export const deleteItem = (item) => {
     payload: item,
   };
 };
+
+// to reset the list after submittion
+export const resetList = () => {
+  return {
+    type: RESET_LIST
+  }
+}
